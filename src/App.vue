058b6737ef775f722d1412f1a750504a46cc23f4
andefine <1939866551@qq.com>
2018-08-17 19:06:30 +0800

    vue init webpack vue-zhihu-daily

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+})
