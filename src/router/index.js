d88f228d447e5b035906c51c7b54df8b7e99a355
andefine <1939866551@qq.com>
2018-08-18 14:45:01 +0800

    使用vue-router进入到首页

--- src/router/index.js
+++ src/router/index.js
@@ -5,4 +5,10 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/HomePage')
+    }
+  ]
 })
--- src/views/HomePage/index.vue
+++ src/views/HomePage/index.vue
@@ -1,12 +1,10 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <div class="home-page">首页</div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+
 }
 </script>
 
