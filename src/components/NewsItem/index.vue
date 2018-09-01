f795c9d0fd0a8a0ae6ad5b62b7fa4aba06055991
andefine <1939866551@qq.com>
2018-09-02 02:49:57 +0800

    fix 修改新闻项如果没有小图展示的样式

--- src/components/NewsItem/index.vue
+++ src/components/NewsItem/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="news-item" @click="$emit('click-to')">
     <span class="title">{{story.title}}</span>
-    <div class="right">
-      <img class="img" v-lazy="story.images ? image403(story.images[0]) : ''" alt="">
+    <div class="right" v-if="story.images">
+      <img class="img" v-lazy="image403(story.images[0])" alt="">
       <div class="corner" v-if="story.multipic">
         <img class="mutiple" src="../../assets/img/images_icon.png" alt="">
         <span>多图</span>
@@ -26,12 +26,13 @@ export default {
 <style lang="scss" scoped>
   .news-item {
     width: 716px;
-    height: 203px;
+    // height: 203px;
+    min-height: 150px;
     box-sizing: border-box;
     background: #fff;
     border-radius: 6px;
     box-shadow: 0 4px 10px 0 #ddd;
-    padding: 24px 34px 0;
+    padding: 24px 34px 28px;
     display: flex;
     justify-content: space-between;
     .title {
