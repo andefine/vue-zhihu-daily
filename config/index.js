3d2c3816ecf4e7188a0234c137ae9e6a34f6d4ad
andefine <1939866551@qq.com>
2018-09-02 02:36:05 +0800

    fix 修改开发环境中source-map的配置

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ module.exports = {
      */
 
     // https://webpack.js.org/configuration/devtool/#development
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'cheap-module-source-map',
 
     // If you have problems debugging vue-files in devtools,
     // set this to false - it *may* help
