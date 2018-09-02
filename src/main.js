634b038de26d75718bf7e2a6818e52976cda933b
andefine <1939866551@qq.com>
2018-09-02 16:57:25 +0800

    按需使用Mint-ui，其实我就使用一个Toast，哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈hhhh或或或或或或或或或或或或或或或或或或或或或

--- src/main.js
+++ src/main.js
@@ -5,11 +5,14 @@ import App from './App'
 import VueLazyload from 'vue-lazyload'
 import router from './router'
 import store from './store'
+import { Toast } from 'mint-ui' // 就使用这一个组件
 import 'normalize.css' // A modern alternative to CSS resets
 import 'lib-flexible' // 引入淘宝手机端适配方案，当然还要使用px2rem-loader
 import '@/styles/common.scss' // 全局公共样式
 
 Vue.config.productionTip = false
+
+Vue.prototype.$toast = Toast
 
 Vue.use(VueLazyload)
 
