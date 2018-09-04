bf2e860e620c8608e4a62ce46ff9e03d505fa101
andefine <1939866551@qq.com>
2018-09-04 21:17:11 +0800

    fix 更改开发环境下的source-map模式

--- config/index.js
+++ config/index.js
@@ -66,7 +66,8 @@ module.exports = {
 
     productionSourceMap: true,
     // https://webpack.js.org/configuration/devtool/#production
-    devtool: '#source-map',
+    // devtool: '#source-map',
+    devtool: 'cheap-module-source-map',
 
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
